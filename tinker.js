42a0c7462eb63ae762b711f3bd2e5d5b5c0cffc7
Aleksandr Ronzhin <ronzhin_as@energomash.ru>
2022-05-31 21:16:37 +0300

    Rename .css, .js to index and rename public to index

--- tinker.js
+++ tinker.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const tinker = express();
 
-tinker.use(express.static(__dirname + '/public'));
+tinker.use(express.static(__dirname + '/index'));
 tinker.use(express.static(__dirname + '/components'));
 tinker.use(express.static(__dirname + '/media'));
 
 tinker.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index/index.html');
 });
 
 tinker.use(function(req, res) {
